ccad1183544e2c8248e569af70641584a21621d4
Erinna Chen <erinna.chen@gmail.com>
2016-03-22 22:29:19 -0600

    Add admin events view with editing

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,4 @@
 class Admin::UsersController < Admin::BaseController
-  def show
-    @orders = Order.all
-    @order_products = OrderProduct.all
-    flash.now[:info] = current_user.admin_message.sample
-  end
-
   def index
-    @orders = Order.all
   end
 end
