f3679886d473ac3c87ce57f6229ba52cf3c40e40
Erinna Chen <erinna.chen@gmail.com>
2016-03-25 17:57:08 -0600

    Add incorrect login credentials test
    * Added alert_error styling rule: red flash message

--- spec/features/admin/admin_can_login_and_view_dashboard_spec.rb
+++ spec/features/admin/admin_can_login_and_view_dashboard_spec.rb
@@ -1,24 +1,44 @@
 require "rails_helper"
 
 RSpec.feature "AdminCanLoginAndViewDashboard", type: :feature do
-  scenario "Admin sees dashboard when they successfully login" do
-    admin = User.create(first_name: "John",
-                        last_name: "Adams",
-                        fullname: "John Adams",
-                        email: "admin@example.com",
-                        password: "password",
-                        role: 1)
-
+  before(:each) do
+    @admin = User.create(first_name: "John",
+                         last_name: "Adams",
+                         fullname: "John Adams",
+                         email: "admin@example.com",
+                         password: "password",
+                         role: 1)
     visit root_path
-
     click_on "sign in"
+  end
 
-    fill_in "email", with: admin.email
-    fill_in "password", with: admin.password
+  scenario "sees dashboard when they successfully login" do
+    fill_in "email", with: @admin.email
+    fill_in "password", with: @admin.password
     click_on "login"
 
     expect(current_path).to eq(admin_dashboard_path)
 
     expect(page).to have_content("Hi John, welcome to ticketcacher!")
+  end
+
+  context "wrong login credentials" do
+    scenario "sees error if email not correct" do
+      fill_in "email", with: "wrongemail@example.com"
+      fill_in "password", with: @admin.password
+      click_on "login"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Sorry, friend.  Something went wrong :(... Please try again.")
+    end
+
+    scenario "sees error if password not correct" do
+      fill_in "email", with: @admin.email
+      fill_in "password", with: "incorrectpassword"
+      click_on "login"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Sorry, friend.  Something went wrong :(... Please try again.")
+    end
   end
 end
