80fd0faebb1a44d131cd85e03d85a24ab47c893a
Erinna Chen <erinna.chen@gmail.com>
2016-03-26 17:53:03 -0600

    Add feature test user status can go from suspended to active

--- spec/features/admin/users/admin_can_change_user_status_spec.rb
+++ spec/features/admin/users/admin_can_change_user_status_spec.rb
@@ -9,6 +9,10 @@ RSpec.feature "AdminCanChangeUserStatuses", type: :feature do
                          email:      "admin@example.com",
                          password:   "password",
                          role: 1)
+    @user2 = User.create(fullname: "Jane Adams",
+                         email: "janeadams@example.com",
+                         password: "password",
+                         status: "suspended")
     ApplicationController.any_instance.stub(:current_user) { @admin }
   end
 
@@ -31,6 +35,29 @@ RSpec.feature "AdminCanChangeUserStatuses", type: :feature do
     within(".users") do
       expect(page).to have_content @user.name
       expect(page).to have_content @user.id
+    end
+  end
+
+  scenario "see user on active users page" do
+    visit "/admin/dashboard"
+    click_on "users"
+    click_on "suspended users"
+
+    within("#user-#{@user2.id}") do
+      select "active", from: "user_status"
+      click_on "update status"
+    end
+
+    expect(page).to have_content "Jane Adams's status has been changed to active"
+    within(".users") do
+      expect(page).to have_content @user2.name
+      expect(page).to have_content @user2.id
+    end
+
+    click_on "suspended users"
+    within(".users") do
+      expect(page).to_not have_content @user2.name
+      expect(page).to_not have_content @user2.id
     end
   end
 end
