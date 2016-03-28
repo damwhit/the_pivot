87eb0c9fdab175c2da3e196dd62b80c1f64b9b2c
Erinna Chen <erinna.chen@gmail.com>
2016-03-27 22:29:52 -0600

    Add sad paths for category creation and update

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,7 +13,20 @@ class Admin::CategoriesController < Admin::BaseController
       flash[:info] = "Created category: #{@category.name}"
       redirect_to admin_categories_path
     else
+      flash[:error] = "Sorry, could not create that new category."
       render "admin/categories/new"
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:info] = "Updated category: #{@category.name}"
+      redirect_to admin_categories_path
+    else
+      flash[:error] = "sorry, unable to update category"
+      @categories = Category.all
+      render "admin/categories/index"
     end
   end
 
--- spec/features/admin/admin_can_create_a_new_category_spec.rb
+++ spec/features/admin/admin_can_create_a_new_category_spec.rb
@@ -20,6 +20,38 @@ RSpec.feature "AdminCanCreateANewCategory", type: :feature do
     end
   end
 
+  context "duplicate category name" do
+    scenario "sees creation form and error message" do
+      create_and_stub_admin
+      category1
+
+      visit "/admin/dashboard"
+      click_on "categories"
+      click_on "new category"
+
+      fill_in "name", with: "festivals"
+      click_on "create category"
+
+      expect(page).to have_content "Sorry, could not create that new category."
+    end
+  end
+
+  context "empty category name" do
+    scenario "sees creation form and error message" do
+      create_and_stub_admin
+      category1
+
+      visit "/admin/dashboard"
+      click_on "categories"
+      click_on "new category"
+
+      fill_in "name", with: ""
+      click_on "create category"
+
+      expect(page).to have_content "Sorry, could not create that new category."
+    end
+  end
+
   context "guest user" do
     scenario "gets 404 page" do
       visit "admin/categories/new"
