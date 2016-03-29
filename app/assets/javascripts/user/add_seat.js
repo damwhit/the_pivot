613aa08860348f807ac9f19f2e4439ab9eef3a3f
David Whitaker <damwhit@gmail.com>
2016-03-28 23:22:41 -0600

    closer but still not able to delete populated rows

--- app/assets/javascripts/user/add_seat.js
+++ app/assets/javascripts/user/add_seat.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
   });
 
   $('#add-seat-edit').click(function() {
-    $('#seat_number_' + seats).after("<tr id='seat_number_" + (seats+1) + "'><td><input type='text' name='seat[]' id='seat_number_" + (seats+1) + "' required='required' placeholder='seat number " + (seats+1) + " ' class='form-control text-input'></td><td class='remove-seat'><a class='btn btn-danger' href='#'>Remove Seat</a></td></tr>");
+    $('#seat_number_1').after("<tr id='seat_number_1', class='ticket'><td><label class='sr-only' for='seat'>Seat</label><input type='text' name='seat[]' id='seat' required='required' class='form-control text-input'></td><td class='remove-seat'><a class='btn btn-danger' href='#'>Remove Seat</a></td></tr>");
     seats++;
   });
 });
