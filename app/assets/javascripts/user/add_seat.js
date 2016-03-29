e6a03043bbea1a5bdf49a798718e47ce71ad4ff9
David Whitaker <damwhit@gmail.com>
2016-03-28 22:58:38 -0600

    jquery almost working
    can append rows correctly, however delete row button does not work on appended rows

--- app/assets/javascripts/user/add_seat.js
+++ app/assets/javascripts/user/add_seat.js
@@ -5,4 +5,9 @@ $( document ).ready(function() {
     $('#seat_number_' + seats).after("<br><input type='text' name='seat[]' id='seat_number_" + (seats+1) + "' required='required' placeholder='seat number " + (seats+1) + " ' class='form-control text-input'>");
     seats++;
   });
+
+  $('#add-seat-edit').click(function() {
+    $('#seat_number_' + seats).after("<tr id='seat_number_" + (seats+1) + "'><td><input type='text' name='seat[]' id='seat_number_" + (seats+1) + "' required='required' placeholder='seat number " + (seats+1) + " ' class='form-control text-input'></td><td class='remove-seat'><a class='btn btn-danger' href='#'>Remove Seat</a></td></tr>");
+    seats++;
+  });
 });
