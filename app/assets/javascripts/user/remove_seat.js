0083ae40455aa8d379784105f031459fedf17b93
David Whitaker <damwhit@gmail.com>
2016-03-29 13:09:13 -0600

    user can edit a listing on both show and index pages
    Will probably want to eventually mess with the styling but a user is now able to edit a listing on both the listings index page as well as the show page for a specific listing.

--- app/assets/javascripts/user/remove_seat.js
+++ app/assets/javascripts/user/remove_seat.js
@@ -1,9 +1,19 @@
+// $( document ).ready(function() {
+//   $(".remove-seat").click(function() {
+//     if ($(this).parent().siblings().length > 0) {
+//       $(this).parent().remove();
+//     } else {
+//       alert("you can't delete the last seat!")
+//     }
+//   })
+// })
 $( document ).ready(function() {
-  $(".remove-seat").click(function() {
+  $("tbody").on("click",".remove-seat", function() {
     if ($(this).parent().siblings().length > 0) {
       $(this).parent().remove();
     } else {
-      alert("you can't delete the last seat!")
+      console.log("help me");
+      alert("you can't delete the last seat!");
     }
   })
 })
