aa69c7c2673eb6990d0555117fc94ee227873739
David Whitaker <damwhit@gmail.com>
2016-03-30 15:10:35 -0600

    concerts festivals sports and popup seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ comedy = Category.find_or_create_by!(name:"comedy")
 conventions = Category.find_or_create_by!(name:"conventions")
 
 @venues = []
-50. times do
+100. times do
   venue_names = ["Arena", "Stadium", "Center", "Theatre", "Palace", "Center for Children Who Can't Read Good"]
 
   name = Faker::StarWars.character
@@ -23,75 +23,268 @@ end
 365.times do
   @event_times << Faker::Time.between(DateTime.now, DateTime.now + 365)
 end
-festivals.events.create(
-  name: "Balloon Fun",
-  venue_id: @venues.sample.id,
-  image: open("https://s3.amazonaws.com/ticket-cacher/categories/air_balloons.jpg"),
-  time: @event_times.sample,
-  status: "active")
 
-festivals.events.create(
-  name: "Giant Bubbles",
-  venue_id: @venues.sample.id,
-  image: open("https://s3.amazonaws.com/ticket-cacher/categories/bubble+boy+festival.jpg"),
-  time: @event_times.sample,
-  status: "active")
-
-festivals.events.create(
-  name: "Cooking Is Kewl",
-  venue_id: @venues.sample.id,
-  image: open("https://s3.amazonaws.com/ticket-cacher/categories/cooking+festival.jpg"),
-  time: @event_times.sample,
-  status: "active")
-
-festivals.events.create(
-  name: "Fun Festival",
-  venue_id: @venues.sample.id,
-  image: open("https://s3.amazonaws.com/ticket-cacher/categories/fun+festival.jpg"),
-  time: @event_times.sample,
-  status: "active")
-
-festivals.events.create(
-  name: "River Boat festival",
-  venue_id: @venues.sample.id,
-  image: open("https://s3.amazonaws.com/ticket-cacher/categories/river+boat+festival.jpeg"),
-  time: @event_times.sample,
-  status: "active")
-
-festivals.events.create(
-  name: "Sunset Music Festival",
-  venue_id: @venues.sample.id,
-  image: open("https://s3.amazonaws.com/ticket-cacher/categories/sunset+festival.jpg"),
-  time: @event_times.sample,
-  status: "active")
-
-festivals.events.create(
-  name: "Computer Music Festival",
-  venue_id: @venues.sample.id,
-  image: open("https://s3.amazonaws.com/ticket-cacher/categories/computer+music.jpg"),
-  time: @event_times.sample,
-  status: "active") 
-
-concerts.events.create(
-  name: "Muppet Rock",
-  venue_id: @venues.sample.id,
-  image: open("https://s3.amazonaws.com/ticket-cacher/categories/muppets_rocking.jpg"),
-  time: @event_times.sample)
-
-9.times do
-  sports.events.create(
-    name: "Basketball Gurlz",
+6.times do
+  festivals.events.create(
+    name: "Yazz Festival",
     venue_id: @venues.sample.id,
-    image: open("https://s3.amazonaws.com/ticket-cacher/categories/olympic_basketball.jpg"),
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/Beaches-Jazz-Festival-Toronto.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Bumba-meu-boi Festival",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/Bumba-meu-boi+Festival.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Festival of Lights",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/Lyon+light+festival.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Marinduque festival",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/MARINDUQUE_dazzles+festival.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Festival of Marinduque",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/MARINDUQUE_festival.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Nilayagan Festivaol of Prosperidad",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/NILAYAGAN+FESTIVAL+OF+PROSPERIDAD.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Beer Fest!!!",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/beer+festival.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Festival of People in Bubbles",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/bubble+boy+festival.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Computer Music Festival",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/computer+music.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Elephant Festival",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/elephant+festival.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Fun Festival",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/fun+festival.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Moon Festival",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/moon+festival.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  festivals.events.create(
+    name: "Sunset Festival",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/festival/sunset+festival.jpg"),
     time: @event_times.sample,
     status: "active")
 end
 
-9.times do
+6.times do
+concerts.events.create(
+  name: "The Beetles",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/beetles+concert.jpeg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "Queen B",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/beyonce+concery.jpeg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "Chris Brown",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/chris+brown+concert.jpeg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "David Bowie",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/davidbowie+concert.jpg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "Drizzy Drake",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/drake+concert.jpeg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "Hova",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/jay+z+concert.jpeg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "Kpop Boyzzzz",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/kpop+boyz.jpg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "Gaga Ooo Lala",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/lady+gaga+concert.jpeg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "Muppet Rock",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/muppets+rocking.jpg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "Sir Paul McCartney",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/paul+mccartney+concert.jpg"),
+  time: @event_times.sample)
+concerts.events.create(
+  name: "Vanessa Carlton",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/concerts/vanessa-carlton_concert.jpg"),
+  time: @event_times.sample)
+end
+
+6.times do
+  sports.events.create(
+    name: "USA Basketball",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/Olympic+Basketball.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  sports.events.create(
+    name: "Game with Shuttlecock",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/badminton+sports.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  sports.events.create(
+    name: "Game with Ball and Foot",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/game+with+ball+and+foot+sports.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  sports.events.create(
+    name: "Game with Ball",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/game+with+ball.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  sports.events.create(
+    name: "Game with Bow",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/game+with+bow+and+arrow+sports.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  sports.events.create(
+    name: "Game with Stick and Puck",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/game+with+stick+and+puck+sports.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  sports.events.create(
+    name: "Heat at Clippers",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/heat+at+clippers+sports.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  sports.events.create(
+    name: "Basketball Gurlz",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/olympic_basketball.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  sports.events.create(
+    name: "Surfs up Brehhhhhh",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/surf+competetion+sports.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  sports.events.create(
+    name: "Game with Racket",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/sports/serena+williams+match+sports.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+end
+
+6.times do
+  popups.events.create(
+    name: "Arugla Time",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/arugula+time.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  popups.events.create(
+    name: "All Things Avacado",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/avacado.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  popups.events.create(
+    name: "Benedicts fa Dayzzz",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/benedicts+fa+days.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  popups.events.create(
+    name: "Fancy Stuff with Eggs",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/egg+things.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  popups.events.create(
+    name: "All of the plates of all of the food",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/food+plates.jpg"),
+    time: @event_times.sample,
+    status: "active")
   popups.events.create(
     name: "Veggie Means Healthy",
     venue_id: @venues.sample.id,
-    image: open("https://s3.amazonaws.com/ticket-cacher/categories/green_beans_and_carrots.jpg"),
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/green_beans_and_carrots.jpg"),
+    time: @event_times.sample,
+    status: "active")
+  popups.events.create(
+    name: "Sandwiches and stoof",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/sandwich+pop+up.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  popups.events.create(
+    name: "Waffle time!",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/waffle+pop+up.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  popups.events.create(
+    name: "Watawatamelonnnnn",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/watawatamelonnnnn.jpeg"),
+    time: @event_times.sample,
+    status: "active")
+  popups.events.create(
+    name: "I'm on a river boat, take a look at me!",
+    venue_id: @venues.sample.id,
+    image: open("https://s3.amazonaws.com/ticket-cacher/categories/pop-ups/river+boat+festival.jpeg"),
     time: @event_times.sample,
     status: "active")
 end
