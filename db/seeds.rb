b2946bab131351552fca42f241704da962fdfd31
David Whitaker <damwhit@gmail.com>
2016-03-30 11:17:13 -0600

    venues are random for seed file
    venues have random city and states and a starwars name followed by arena, stadium etc.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,46 +5,54 @@ popups = Category.find_or_create_by!(name:"popups")
 comedy = Category.find_or_create_by!(name:"comedy")
 conventions = Category.find_or_create_by!(name:"conventions")
 
-venue1 = Venue.find_or_create_by!(
-  name: "Outside",
-  city: "Lima",
-  state: "Ohio")
-venue2 = Venue.find_or_create_by!(
-  name: "Stadium",
-  city: "Atlanta",
-  state: "Kansas")
-venue3 = Venue.find_or_create_by!(
-  name: "Convention Hall",
-  city: "St. Louis",
-  state: "Georgia")
-venue4 = Venue.find_or_create_by!(
-  name: "Sports Stadium",
-  city: "New York",
-  state: "Utah")
-venue5 = Venue.find_or_create_by!(
-  name: "Hipster Bathouse",
-  city: "Horace's Lair",
-  state: "Beard")
+@venues = []
+50. times do
+  venue_names = ["Arena", "Stadium", "Center", "Theatre", "Palace", "Center for Children Who Can't Read Good"]
 
-9.times do
-  festivals.events.create(
-    name: "Balloon Fun",
-    venue_id: venue1.id,
-    image: open("https://s3.amazonaws.com/ticket-cacher/categories/air_balloons.jpg"),
-    time: "March 02, 2017 16:00",
-    status: "active")
+  name = Faker::StarWars.character
+  city = Faker::Address.city
+  state = Faker::Address.state
+
+  @venues << Venue.find_or_create_by!(
+              name: "#{name} #{venue_names.sample}",
+              city: city,
+              state: state)
 end
+
+event_time = Faker::Time.between(DateTime.now, DateTime.now + 365)
+
+festivals.events.create(
+  name: "Balloon Fun",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/air_balloons.jpg"),
+  time: event_time,
+  status: "active")
+
+festivals.events.create(
+  name: "Balloon Fun",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/air_balloons.jpg"),
+  time: event_time,
+  status: "active")
+
+festivals.events.create(
+  name: "Balloon Fun",
+  venue_id: @venues.sample.id,
+  image: open("https://s3.amazonaws.com/ticket-cacher/categories/air_balloons.jpg"),
+  time: event_time,
+  status: "active")
+
 
 concerts.events.create(
   name: "Muppet Rock",
-  venue_id: venue2.id,
+  venue_id: @venues.sample.id,
   image: open("https://s3.amazonaws.com/ticket-cacher/categories/muppets_rocking.jpg"),
   time: "November 20, 2020 18:00", status: "active")
 
 9.times do
   sports.events.create(
     name: "Basketball Gurlz",
-    venue_id: venue4.id,
+    venue_id: @venues.sample.id,
     image: open("https://s3.amazonaws.com/ticket-cacher/categories/olympic_basketball.jpg"),
     time: "July 10, 2016 19:00",
     status: "active")
@@ -53,7 +61,7 @@ end
 9.times do
   popups.events.create(
     name: "Veggie Means Healthy",
-    venue_id: venue5.id,
+    venue_id: @venues.sample.id,
     image: open("https://s3.amazonaws.com/ticket-cacher/categories/green_beans_and_carrots.jpg"),
     time: "July 10, 2016 19:00",
     status: "active")
@@ -62,7 +70,7 @@ end
 9.times do
   comedy.events.create(
     name: "Louis C.K.",
-    venue_id: venue3.id,
+    venue_id: @venues.sample.id,
     image: open("https://s3.amazonaws.com/ticket-cacher/categories/louis_ck.jpg"),
     time: "July 10, 2016 19:00",
     status: "active")
@@ -71,7 +79,7 @@ end
 9.times do
   conventions.events.create(
     name: "Cosplay Extravaganza",
-    venue_id: venue3.id,
+    venue_id: @venues.sample.id,
     image: open("https://s3.amazonaws.com/ticket-cacher/categories/comicon.jpg"),
     time: "July 10, 2016 19:00",
     status: "active")
